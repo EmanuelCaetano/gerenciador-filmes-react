b1e4ba15262f82b448e855bfab21285664d46338
Emanuel Caetano <emanuel.caetano.92@outlook.com>
2023-01-30 16:43:12 -0300

    criacao da topbanner com estilizaçao

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { Topbar } from "./componentes/topbar";
+import {Topbanner} from "./componentes/topbanner";
 import "./global.css";
 
 function App() {
   return (
     <>
     <Topbar/>
+    <Topbanner />
     </>
+
   );
 }
 
