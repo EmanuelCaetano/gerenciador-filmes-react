d4517d39ec91b12f4726eda49ccf57e5a0d68544
Emanuel Caetano <emanuel.caetano.92@outlook.com>
2023-01-30 17:09:33 -0300

    adicao de logo responsivel

--- src/componentes/topbanner/index.jsx
+++ src/componentes/topbanner/index.jsx
@@ -1,8 +1,10 @@
+import Google from "./imgs/logo.png";
 import "./style.css";
 
 export function Topbanner(){
   return(
     <div  className="topbanner">
+      <img src={Google} className="logo" alt="Google"/>
       <center className="topbanner">GERENCIADOR DE FILMES</center>     
     </div>
   );
