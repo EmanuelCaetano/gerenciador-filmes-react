cb65991e000efb10f82e0fd8f1230acd6bfe8c80
Emanuel Caetano <emanuel.caetano.92@outlook.com>
2023-01-31 01:58:10 -0300

    adição da pagina home

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import {  Topbanner } from "./componentes/topbanner";
 import {  Mainbody  } from "./componentes/mainbody";
 import {  Footer  } from "./componentes/footer";
 import {  Menu  } from "./componentes/menu";
+import { Home } from "./pages/home";
 import "./global.css";
 
 function App() {
@@ -13,8 +14,10 @@ function App() {
       {/*importando uma pagina dentro da outra*/}
       <Mainbody>
           <Menu />
+          <Home/>
       </Mainbody>
       {/*finalizando da importação*/}
+      
     <Footer />
     </>
 
---
+++ src/pages/home/index.jsx
@@ -0,0 +1,11 @@
+import "./style.css";
+
+export function Home (){
+  return (
+    <div className="home">
+      <h1 className="home">Home</h1>
+      <div>Conteudo</div>
+    </div>
+
+  );
+}
