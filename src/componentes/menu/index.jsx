53ff2137b711e5c9688b6157d49fd7e208982bcd
Emanuel Caetano <emanuel.caetano.92@outlook.com>
2023-01-31 01:10:57 -0300

    Insercao de links e estilazao do menu

--- src/componentes/menu/index.jsx
+++ src/componentes/menu/index.jsx
@@ -1,7 +1,27 @@
+import { Link } from "react-router-dom";
+
 import "./style.css";
 
 export function Menu(){
   return (
-      <div>Home</div>
+      <div className="menu">
+       
+      <nav className="menu">
+          <Link to="/" className="menu">HOME</Link>
+      </nav>
+
+      <nav className="menu">
+         <Link to="/" className="menu">FILMES</Link>
+      </nav>
+
+      <nav className="menu">
+          <Link to="/" className="menu">DIRETORES</Link>
+      </nav>
+
+      <nav className="menu">
+          <Link to="/" className="menu">ATORES</Link>
+      </nav>
+      
+      </div>
   );
 }
