0d9bb892c8ef53f7639d414a0d2ab86f6acdbd67
Emanuel Caetano <emanuel.caetano.92@outlook.com>
2023-02-03 23:03:58 -0300

    utilizando usestate

--- src/pages/actors/index.jsx
+++ src/pages/actors/index.jsx
@@ -1,12 +1,35 @@
 import "./style.css";
 import { BACKEND_URL } from "../../config";
 import  axios  from "axios";
+import { useState} from "react";
+
+function actorsHandler(actors){
+
+  if(!Array.isArray(actors)){
+    return actors;
+
+    }
+
+  var i = 0;
+
+  var content = actors.map(function(actor){
+        return(
+          <div key={i++}> {actor.nome} </div>
+        );
+  });
+
+ return content;
+}
 
 export function Actors (){
 
+  const [actors, setActors] = useState("Nada a mostrar");
+
+  
+
   axios.get(BACKEND_URL + "/atores")
   .then(function(response){
-
+        setActors(actorsHandler(response.data));
   })
   .catch(function(error){
 
@@ -18,7 +41,8 @@ export function Actors (){
   return (
     <div className=".content">
     <h1>Atores</h1>
-      <div>Conteudo</div>
+      
+      {actors}
     </div>
 
   );
