04520f4ba14edb3c014befc84c1c2b091b3d7d67
Emanuel Caetano <emanuel.caetano.92@outlook.com>
2023-02-15 23:01:28 -0300

    trazendo filmes e diretores

--- src/pages/directors/index.jsx
+++ src/pages/directors/index.jsx
@@ -1,10 +1,50 @@
 import "./style.css";
+import { BACKEND_URL } from "../../config";
+import  axios  from "axios";
+import { useState , useEffect} from "react";
+
+function directorsHandler(directors){
+
+  if(!Array.isArray(directors)){
+    return directors;
+
+    }
+
+  var i = 0;
+
+  var content = directors.map(function(director){
+        return(
+          <div className="directors-content" key={i++}>{i+1} - {director.nome} </div>
+        );
+  });
+
+ return content;
+}
+
+
 
 export function Directors (){
+
+  const  [directors, setDirectors] = useState("Nada a mostrar");
+
+  useEffect(function(){
+    axios.get(BACKEND_URL + "/directors")
+  .then(function(response){
+        setDirectors(directorsHandler(response.data));
+  })
+  .catch(function(error){
+
+  })
+  .finally(function(){
+
+  });
+
+  },[]);  
+
   return (
     <div className=".content">
       <h1>Diretores</h1>
-      <div>Conteudo</div>
+      {directors}
     </div>
 
   );
--- src/pages/films/index.jsx
+++ src/pages/films/index.jsx
@@ -1,10 +1,48 @@
 import "./style.css";
+import { BACKEND_URL } from "../../config";
+import  axios  from "axios";
+import { useState , useEffect} from "react";
+
+function filmsHandler(films){
+
+  if(!Array.isArray(films)){
+    return films;
+
+    }
+
+  var i = 0;
+
+  var content = films.map(function(film){
+        return(
+          <div className="film-content" key={i++}>{i+1} - {film.nome} </div>
+        );
+  });
+
+ return content;
+}
 
 export function Films (){
+
+  const  [films, setFilms] = useState("Nada a mostrar");
+
+  useEffect(function(){
+    axios.get(BACKEND_URL + "/Filmes")
+  .then(function(response){
+        setFilms(filmsHandler(response.data));
+  })
+  .catch(function(error){
+
+  })
+  .finally(function(){
+
+  });
+
+  },[]);  
+
   return (
     <div className=".content">
       <h1>Filmes</h1>
-      <div>Conteudo</div>
+     {films}
     </div>
 
   );
